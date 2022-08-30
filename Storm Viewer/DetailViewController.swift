2f49e27817253a3b6e63c703d090ffd80558c312
Ali Demirtaş <alidemirtas.7@icloud.com>
2022-08-31 01:25:35 +0300

    done

---
+++ Storm Viewer/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer
+//
+//  Created by Ali Demirtaş on 31.08.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let image = selectedImage {
+            imageView.image = UIImage(named: image)
+        }
+   
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+
+
+}
